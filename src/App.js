2b9c4699dfec2a9dbba004f8efc63bf96993455e
Prathamesh Navalagi <Prathamesh.Navalagi@harbingergroup.com>
2020-03-13 19:13:45 +0530

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
-import { 
-  BrowserRouter as Router,
-  Route
- } from "react-router-dom";
-import Dashboard from "./containers/Dashboard";
-import Register from "./containers/Register"
-import Login from "./containers/Login";
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//       <Router>
-//            <Route exact path="/" component={Dashboard}/>
-//            <Route path="/register" component={Register}/>
-//            <Route path="/login" component={Login}/>
-//       </Router>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-function App() {
-  return (
-    <div className="App">
-      <Login></Login>
-    </div>
-  );
+import Layout from './components/Layout/Layout';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p>test</p>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './Toolbar.css';
+const toolbar = (props) => (
+    <header className={classes.Toolbar}>
+        <div>HOME</div>
+        <div>ABOUT US</div>
+        <div>PRICING</div>
+        <div>OUR OFFERS</div>
+        <div>CONTACT US</div>
+        <div>LOGIN</div>
+        <div>SIGN UP</div>
+        <nav>
+            ...
+        </nav>
+    </header>
+);
+
+export default toolbar;
